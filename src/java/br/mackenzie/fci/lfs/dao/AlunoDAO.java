dd5b4a3b7981f5f677a51209ebe3ecb90a425604
Rubens Akio <rubens.akio.mac@gmail.com>
2017-07-05 09:55:36 -0300

    modifiquei a classe AlunoDAO

--- src/java/br/mackenzie/fci/lfs/dao/AlunoDAO.java
+++ src/java/br/mackenzie/fci/lfs/dao/AlunoDAO.java
@@ -21,35 +21,37 @@ import java.util.logging.Logger;
  * @author LFS
  */
 public class AlunoDAO implements GenericoDAO<Aluno> {
-
+    
     @Override
     public void inserir(Aluno aluno) {
-
+        
         try {
-            String sql = "INSERT INTO lfs.aluno (nome,email,telefone) values(?,?,?)";
+            String sql = "INSERT INTO lfs.aluno (nome,email,telefone,numMatricula) values(?,?,?,?)";
             Connection c = Conexao.getInstance().getConnection();
             PreparedStatement ps = c.prepareStatement(sql);
             ps.setString(1, aluno.getNome());
             ps.setString(2, aluno.getEmail());
             ps.setString(3, aluno.getTelefone());
+            ps.setInt(4, aluno.getNumMatricula());
             ps.execute();
             c.close();
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(AlunoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
     }
-
+    
     @Override
     public void atualizar(Aluno aluno) {
         try {
-            String sql = "UPDATE lfs.aluno SET nome=?, email=?, telefone=? where idAluno=?";
+            String sql = "UPDATE lfs.aluno SET nome=?, email=?, telefone=?, numMatricula=? where idAluno=?";
             Connection c = Conexao.getInstance().getConnection();
             PreparedStatement ps = c.prepareStatement(sql);
             ps.setString(1, aluno.getNome());
             ps.setString(2, aluno.getEmail());
             ps.setString(3, aluno.getTelefone());
-            ps.setInt(4, aluno.getCodAluno());
+            ps.setInt(4, aluno.getNumMatricula());
+            ps.setInt(5, aluno.getCodAluno());
             ps.execute();
             c.close();
         } catch (ClassNotFoundException ex) {
@@ -57,11 +59,11 @@ public class AlunoDAO implements GenericoDAO<Aluno> {
         } catch (SQLException ex) {
             Logger.getLogger(AlunoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
     }
-
+    
     public Aluno listarPorId(Aluno aluno) {
-
+        
         try {
             String sql = "SELECT * FROM lfs.aluno  WHERE idAluno = ?";
             Connection c = Conexao.getInstance().getConnection();
@@ -69,21 +71,22 @@ public class AlunoDAO implements GenericoDAO<Aluno> {
             ps.setInt(1, aluno.getCodAluno());
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-
+                
                 aluno.setCodAluno(rs.getInt("idAluno"));
                 aluno.setNome(rs.getString("nome"));
                 aluno.setEmail(rs.getString("email"));
                 aluno.setTelefone(rs.getString("telefone"));
-
+                aluno.setNumMatricula(rs.getInt("numMatricula"));
+                
             }
-
+            
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(AlunoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
         return aluno;
     }
-
+    
     @Override
     public void remover(Aluno aluno) {
         try {
@@ -99,33 +102,34 @@ public class AlunoDAO implements GenericoDAO<Aluno> {
             Logger.getLogger(AlunoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    
     @Override
     public List<Aluno> consultar() {
-
+        
         List<Aluno> alunos = new ArrayList<Aluno>();
         try {
             Connection connection = Conexao.getInstance().getConnection();
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery("select * from lfs.aluno "
                     + "as a");
-
+            
             while (result.next()) {
-
+                
                 alunos.add(new Aluno(result.getInt("idAluno"),
                         result.getString("nome"),
                         result.getString("email"),
-                        result.getString("telefone")));
-
+                        result.getString("telefone"),
+                        result.getInt("numMatricula")));
+                
             }
         } catch (ClassNotFoundException ex) {
-
+            
             Logger.getLogger(AlunoDAO.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(AlunoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
         return alunos;
     }
-
+    
 }
