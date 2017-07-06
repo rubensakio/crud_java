17565e5932588ff25ba5d870dc24d8bff4c42762
Rubens Akio <rubens.akio.mac@gmail.com>
2017-07-06 14:33:01 -0300

    criando jsp para listar semestre dos alunos e atualizando smest

---
+++ build/web/WEB-INF/jsp/semestre/consultarSemestre.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : consultarSemestre
+    Created on : 06/07/2017, 14:19:48
+    Author     : LFS
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1> Lista de semestre </h1>
+    </body>
+</html>
--- src/java/br/mackenzie/fci/lfs/servlet/SemestreController.java
+++ src/java/br/mackenzie/fci/lfs/servlet/SemestreController.java
@@ -31,19 +31,15 @@ public class SemestreController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SemestreController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SemestreController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        if (request != null && request.getParameter("command") != null) {
+
+            if ("semestre.consultarSemestre".equalsIgnoreCase(request.getParameter("command"))) {
+
+                request.getRequestDispatcher("WEB-INF/jsp/semestre/consultarSemestre.jsp").forward(request, response);
+            }
         }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
