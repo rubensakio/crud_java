4205a8014a883a64716e0d016d5e3f8856a35d4b
Rubens Akio <rubens.akio.mac@gmail.com>
2017-07-24 10:10:56 -0300

    salvando pagina CursoDAO

--- src/java/br/mackenzie/fci/lfs/dao/CursoDAO.java
+++ src/java/br/mackenzie/fci/lfs/dao/CursoDAO.java
@@ -5,10 +5,34 @@
  */
 package br.mackenzie.fci.lfs.dao;
 
+import br.mackenzie.fci.lfs.exception.PersistenciaException;
+import br.mackenzie.fci.lfs.model.Curso;
+import java.util.List;
+
 /**
  *
  * @author LFS
  */
-public class CursoDAO {
+public class CursoDAO implements GenericoDAO<Curso>{
+
+    @Override
+    public void inserir(Curso e) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void atualizar(Curso e) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remover(Curso e) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Curso> consultar() throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
